78de67bc9fd8b6853c166046a0153554fde43656
Marcio Costa <marcio-costa38@hotmail.com>
2024-04-30 21:10:07 -0300

    Criação de entrada e saida

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o seu Nome: ");
+        String nomeDoCliente = scan.nextLine();
+
+        System.out.println("Digite o sua agencia: ");
+        String agencia = scan.nextLine();
+
+        System.out.println("Digite o numero da sua conta: ");
+        int numero = scan.nextInt();
+
+        System.out.println("Digite o seu Saldo: ");
+        double saldo =  scan.nextDouble();
+
+        System.out.println("Olá" + nomeDoCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+        agencia +", conta "+ numero + " e seu saldo " + saldo +" já está disponível para saque");
     }
 }
