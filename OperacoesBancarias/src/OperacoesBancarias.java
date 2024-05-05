862794c97c383e9d4695a65b5d2e3a35f837b107
Marcio Costa <marcio-costa38@hotmail.com>
2024-05-05 10:27:55 -0300

    finalizando logica geral, falta organizar em metodos

--- OperacoesBancarias/src/OperacoesBancarias.java
+++ OperacoesBancarias/src/OperacoesBancarias.java
@@ -9,15 +9,18 @@ public class OperacoesBancarias {
         // Loop infinito para manter o programa em execução até que o usuário decida sair
         while (loopWhile) { 
             int continuar = 0;
-            System.out.println("digite: 1 - Depositar\r\n" + //
+            System.out.println("--------------------------------------------------------------------\r\n"+
+                        "Digite: 1 - Depositar\r\n" + //
                         "2 - Sacar\r\n" + //
                         "3 - Consultar Saldo\r\n" + //
                         "4 - Encerrar");
 
+            System.out.print("Opção escolhida: ");
             int opcao = scanner.nextInt();
                         
             switch (opcao){ 
                 case 1:
+                    //Depositar
                     System.out.println("Digite o valor a ser depositado: ");
                     double depósito = scanner.nextDouble();
                     saldo += depósito;
@@ -37,6 +40,7 @@ public class OperacoesBancarias {
                         System.out.println("Opção não existente");
                     }
                 case 2:
+                    //Sacar
                     double saque;
                     System.out.println("Digite o valor a ser sacado: ");
                     saque = scanner.nextDouble();
@@ -47,16 +51,49 @@ public class OperacoesBancarias {
                         System.out.printf("Valor do Saque: R$ %.2f\n",saque);
                         System.out.printf("Saldo Atual: %.2f\n", saldo);
                     }
+                    
+                    System.out.println("Deseja encerrar o programa?\r\n" +
+                                        "1 - Sim\r\n" +
+                                        "2 - Não");
+                    continuar = scanner.nextInt();
+                    if (continuar == 1){
                     loopWhile = false;
                     break;
+                    } else if(continuar == 2) {
+                        continue;
+                    }else {
+                        System.out.println("Opção não existente");
+                    }
                 case 3:
-                    System.out.printf("O Saldo atual da conta é de: R$ %.2f", saldo);
+                    //Ver o Saldo
+                    System.out.printf("O Saldo atual da conta é de: R$ %.2f\r\n", saldo);
+                    
+                    System.out.println("Deseja encerrar o programa?\r\n" +
+                                        "1 - Sim\r\n" +
+                                        "2 - Não");
+                    continuar = scanner.nextInt();
+                    if (continuar == 1){
                     loopWhile = false;
                     break;
+                    } else if(continuar == 2) {
+                        continue;
+                    }else {
+                        System.out.println("Opção não existente");
+                    }
                 case 4:
-                    System.out.println("Saindo...");
+                //Encerrar o programa
+                    System.out.println("Deseja encerrar o programa?\r\n" +
+                                        "1 - Sim\r\n" +
+                                        "2 - Não");
+                    continuar = scanner.nextInt();
+                    if (continuar == 1){
                     loopWhile = false;
                     break;
+                    } else if(continuar == 2) {
+                        continue;
+                    }else {
+                        System.out.println("Opção não existente");
+                    }
 
                 // TODO: Implemente as condições necessárias para avaliar a opção escolhida:
                 // Dica: Utilze o switch/case para o programa realizar as operações escolhidas pelo usuário.
