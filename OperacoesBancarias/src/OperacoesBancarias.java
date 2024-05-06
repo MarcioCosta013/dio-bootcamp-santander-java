24638111bbff2979ddd9e542acba36f96aa8b19a
Marcio Costa <marcio-costa38@hotmail.com>
2024-05-06 09:52:44 -0300

    finalizando o primeiro desafiooperations in the banking application. It displays an error

--- OperacoesBancarias/src/OperacoesBancarias.java
+++ OperacoesBancarias/src/OperacoesBancarias.java
@@ -5,102 +5,79 @@ public class OperacoesBancarias {
         Scanner scanner = new Scanner(System.in); 
         double saldo = 0;
         boolean loopWhile = true;
-         
+ 
+        /*System.out.println("--------------------------------------------------------------------\r\n"+
+                        "Digite: \r\n 1 - Depositar\r\n" + //
+                        "2 - Sacar\r\n" + //
+                        "3 - Consultar Saldo\r\n" + //
+                        "4 - Encerrar"); */
+
+        //System.out.print("\nOpção escolhida: ");
         // Loop infinito para manter o programa em execução até que o usuário decida sair
         while (loopWhile) { 
             int continuar = 0;
-            System.out.println("--------------------------------------------------------------------\r\n"+
-                        "Digite: 1 - Depositar\r\n" + //
-                        "2 - Sacar\r\n" + //
-                        "3 - Consultar Saldo\r\n" + //
-                        "4 - Encerrar");
 
-            System.out.print("Opção escolhida: ");
-            int opcao = scanner.nextInt();
+            
+            try {
+                int opcao = scanner.nextInt();
+
+                switch (opcao){ 
+                    case 1:
+                        //Depositar
                         
-            switch (opcao){ 
-                case 1:
-                    //Depositar
-                    System.out.println("Digite o valor a ser depositado: ");
-                    double depósito = scanner.nextDouble();
-                    saldo += depósito;
-                    System.out.printf("Saldo atual: R$ %.2f\n",saldo);
-
-
-                    System.out.println("Deseja encerrar o programa?\r\n" +
-                                        "1 - Sim\r\n" +
-                                        "2 - Não");
-                    continuar = scanner.nextInt();
-                    if (continuar == 1){
-                    loopWhile = false;
-                    break;
-                    } else if(continuar == 2) {
-                        continue;
-                    }else {
-                        System.out.println("Opção não existente");
-                    }
-                case 2:
-                    //Sacar
-                    double saque;
-                    System.out.println("Digite o valor a ser sacado: ");
-                    saque = scanner.nextDouble();
-                    if (saque > saldo){
-                        System.out.println("Saldo insuficiente!");
-                    } else {
-                        saldo -= saque;
-                        System.out.printf("Valor do Saque: R$ %.2f\n",saque);
-                        System.out.printf("Saldo Atual: %.2f\n", saldo);
-                    }
+                        //System.out.println("Digite o valor a ser depositado: ");
+                        
+                        try {
+                            double deposito = scanner.nextDouble();
+                            saldo += deposito;
+                            System.out.printf("Saldo atual: " + saldo +"\n");
+                            break;
+                        } catch (Exception e) {
+                            System.out.println("Erro: Digite só numeros");
+                            break;
+                        }
                     
-                    System.out.println("Deseja encerrar o programa?\r\n" +
-                                        "1 - Sim\r\n" +
-                                        "2 - Não");
-                    continuar = scanner.nextInt();
-                    if (continuar == 1){
-                    loopWhile = false;
-                    break;
-                    } else if(continuar == 2) {
-                        continue;
-                    }else {
-                        System.out.println("Opção não existente");
-                    }
-                case 3:
-                    //Ver o Saldo
-                    System.out.printf("O Saldo atual da conta é de: R$ %.2f\r\n", saldo);
-                    
-                    System.out.println("Deseja encerrar o programa?\r\n" +
-                                        "1 - Sim\r\n" +
-                                        "2 - Não");
-                    continuar = scanner.nextInt();
-                    if (continuar == 1){
-                    loopWhile = false;
-                    break;
-                    } else if(continuar == 2) {
-                        continue;
-                    }else {
-                        System.out.println("Opção não existente");
-                    }
-                case 4:
-                //Encerrar o programa
-                    System.out.println("Deseja encerrar o programa?\r\n" +
-                                        "1 - Sim\r\n" +
-                                        "2 - Não");
-                    continuar = scanner.nextInt();
-                    if (continuar == 1){
-                    loopWhile = false;
-                    break;
-                    } else if(continuar == 2) {
-                        continue;
-                    }else {
-                        System.out.println("Opção não existente");
-                    }
-
-                // TODO: Implemente as condições necessárias para avaliar a opção escolhida:
-                // Dica: Utilze o switch/case para o programa realizar as operações escolhidas pelo usuário.
-                // Exibe mensagem de opção inválida se o usuário escolher uma opção inválida:    
-                default:
-                    System.out.println("Opção inválida. Tente novamente."); 
+                    case 2:
+                        //Sacar
+                        double saque;
+                        //System.out.println("Digite o valor a ser sacado: ");
+                        saque = scanner.nextDouble();
+                        if (saque > saldo){
+                            System.out.println("Saldo insuficiente.");
+                            break;
+                        } else {
+                            saldo -= saque;
+                            System.out.printf("Saldo atual: " + saldo +"\n");
+                            break;
+                        }
+                        
+                        
+                    case 3:
+                        //Consultar Saldo
+                        System.out.printf("Saldo atual: " + saldo +"\n");
+                        break;
+                        
+                    case 0:
+                        //Encerrar o programa
+                        try{
+                            System.out.println("Programa encerrado.");
+                            loopWhile = false;
+                            break;
+                            
+                        }catch (Exception e){
+                            System.out.println("Erro na digitação da opção, por favor tente novamente só com numeros.");
+                        }
+                    // Exibe mensagem de opção inválida se o usuário escolher uma opção inválida:    
+                    default:
+                        System.out.println("Opção inválida. Tente novamente ."); 
+                }
+            } catch (Exception e) {
+                System.err.println("\r\nErro! Opção inválida. Somente Numeros...");
+                break;
             }
+         
         }
+        
     }
+
 }
