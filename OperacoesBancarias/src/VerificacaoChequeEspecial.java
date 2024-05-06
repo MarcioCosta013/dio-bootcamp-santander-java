595324edd3600e1cde66e0cad7d1fb374629c815
Marcio Costa <marcio-costa38@hotmail.com>
2024-05-06 15:40:55 -0300

    finalizado o execicio 4

--- OperacoesBancarias/src/VerificacaoChequeEspecial.java
+++ OperacoesBancarias/src/VerificacaoChequeEspecial.java
@@ -9,18 +9,15 @@ public class VerificacaoChequeEspecial {
         double saque = scanner.nextDouble(); 
         double limiteChequeEspecial = 500; 
 
-        // TODO: Verifique se o saque não ultrapassa o saldo disponível na conta:
-        
-
-            // TODO: Verifique se o saque ultrapassa o limite do cheque especial, mas não o saldo total disponível:
-          
-
-                // Se o saque ultrapassar tanto o saldo disponível quanto o limite do cheque especial
-                System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
-            }
+        if (saque<=saldo){
+            System.out.println("Transacao realizada com sucesso.");
+        } else if((saldo+limiteChequeEspecial)>saque) {
+            System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
+        } else {
+            System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
         }
 
-// Fechamos o objeto Scanner para liberar os recursos:
+        // Fechamos o objeto Scanner para liberar os recursos:
         scanner.close(); 
     }
 }
