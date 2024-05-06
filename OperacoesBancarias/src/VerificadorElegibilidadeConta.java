8e9787a14e0e20a135c79571b42bc72a014cfbd3
Marcio Costa <marcio-costa38@hotmail.com>
2024-05-06 14:08:31 -0300

    Exercicio finalizado

--- OperacoesBancarias/src/VerificadorElegibilidadeConta.java
+++ OperacoesBancarias/src/VerificadorElegibilidadeConta.java
@@ -5,13 +5,26 @@ public class VerificadorElegibilidadeConta {
 
     public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in); 
+        try{
 
-        int idade = scanner.nextInt(); 
+            int idade = scanner.nextInt();
 
-// TODO: Verifique se a idade é maior ou igual a 18 e imprima uma mensagem informando que o usuário é elegível para criar uma conta bancária:
+            if (idade >= 18){
+                System.out.println("Voce esta elegivel para criar uma conta bancaria.");
+            }else {
+                System.out.println("Voce nao esta elegivel para criar uma conta bancaria.");
+            }
+
+        } catch (Exception e){
+            System.out.println("Erro: Somente numeros...");
+        } finally {
+            // Fechamos o objeto Scanner para liberar os recursos:
+            scanner.close();
+        }
+        // TODO: Verifique se a idade é maior ou igual a 18 e imprima uma mensagem informando que o usuário é elegível para criar uma conta bancária:
        
 
-// Fechamos o objeto Scanner para liberar os recursos:
-        scanner.close(); 
+        
+         
     }
 }
