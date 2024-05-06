28c375801911bde1f58d9d03b0f42ab9c588a9ed
Marcio Costa <marcio-costa38@hotmail.com>
2024-05-06 19:38:39 -0300

    exercicio 5 finalizado

--- OperacoesBancarias/src/ControleSimplesDeSaques.java
+++ OperacoesBancarias/src/ControleSimplesDeSaques.java
@@ -7,24 +7,23 @@ public class ControleSimplesDeSaques {
 
         double limiteDiario = scanner.nextDouble();
 
-// TODO: Crie um loop 'for' para iterar sobre os saques:
+        for(double i=0; i<limiteDiario;){
+            double valorSaque = scanner.nextDouble();
 
-// TODO: Solicite ao usuário o valor do saque:
-           
-
-// TODO: Verifique se o valor do saque é zero, encerrando as transações:
-// Dica: Utilize um 'if/else' para verificar as condições do valorSaque e o limiteDiario;
-
-           
-// TODO: Se o valor do saque não ultrapassar o limite diário, subtraia o valor do saque do limite diário:
-            
-
-// TODO: Informe que o saque foi realizado e mostre o limite restante:
-              
+            if (valorSaque == 0) {
+                i = limiteDiario;
+                System.out.println("Transacoes encerradas.");
+                break;
+            } else if(valorSaque > limiteDiario){
+                i = limiteDiario;
+                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+            } else{
+                i = limiteDiario - valorSaque;
+                System.out.println("Saque realizado. Limite restante: " + i);
             }
         }
-
-// Fechamos o Scanner para evitar vazamento de recursos:
+          
+        // Fechamos o Scanner para evitar vazamento de recursos:
         scanner.close(); 
     }
 }
