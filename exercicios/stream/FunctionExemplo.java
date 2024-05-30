f7bce08ec43ba9302c868232ca5c405af9c969d2
Marcio Costa <marcio-costa38@hotmail.com>
2024-05-30 20:05:33 -0300

    iniciando o estudo de Function's

---
+++ exercicios/stream/FunctionExemplo.java
@@ -0,0 +1,9 @@
+package exercicios.stream;
+
+/**
+ * Function<T, R>: Representa uma função que aceita um argumento do tipo T e retorna um resultado do tipo R. 
+ * É utilizada para transformar ou mapear os elementos do Stream em outros valores ou tipos.
+ */
+public class FunctionExemplo {
+    
+}
